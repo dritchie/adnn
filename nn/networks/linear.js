e272e64cdc7a0ddc043a0529f6d560fd1993525e
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-12-07 18:29:47 -0800

    Network that does fully-connected linear between the layers/slices/channels of an image.

--- nn/networks/linear.js
+++ nn/networks/linear.js
@@ -4,6 +4,7 @@ var Network = require('../network.js');
 var assert = require('assert');
 
 
+// Fully connected network
 function LinearNetwork(nIn, nOut, optname) {
 	Network.call(this);
 	this.name = optname || 'linear';
@@ -100,7 +101,141 @@ function linear(nIn, nOut, optname) {
 	return new LinearNetwork(nIn, nOut, optname);
 }
 
-module.exports = {
-	linear: linear
+
+// ----------------------------------------------------------------------------
+
+
+// Fully connected network between the layers (i.e. channels) of an image at 
+//    each pixel
+function LayerwiseLinearNetwork(nIn, nOut, optname) {
+	Network.call(this);
+	this.name = optname || 'layerwiseLinear';
+	this.inSize = nIn;
+	this.outSize = nOut;
+	this.weights = ad.params([nOut, nIn], this.name+'_weights');
+	this.biases = ad.params([nOut], this.name+'_biases');
+	this.parameters = [this.weights, this.biases];
+	this.isTraining = false;
+}
+LayerwiseLinearNetwork.prototype = Object.create(Network.prototype);
+
+LayerwiseLinearNetwork.prototype.setTraining = function(flag) {
+	this.isTraining = flag;
 };
+
+LayerwiseLinearNetwork.prototype.serializeJSON = function() {
+	return {
+		type: 'layerwiseLinear',
+		name: this.name,
+		inSize: this.inSize,
+		outSize: this.outSize,
+		weights: ad.value(this.weights).toFlatArray(),
+		biases: ad.value(this.biases).toFlatArray()
+	};
+}
+Network.deserializers.layerwiseLinear = function(json) {
+	var net = new LayerwiseLinearNetwork(json.inSize, json.outSize, json.name);
+	ad.value(net.weights).fromFlatArray(json.weights);
+	ad.value(net.biases).fromFlatArray(json.biases);
+	return net;
+};
+
+
+var layerwiseMVMulAdd = ad.newFunction({
+	OutputType: Tensor,
+	name: 'layerwiseMVMulAdd',
+	forward: function(img, weights, biases) {
+		img = ad.value(img);
+		weights = ad.value(weights);
+		biases = ad.value(biases);
+		var inD = img.dims[0];
+		if (inD !== weights.dims[1]) {
+			assert(false, 'LayerwiseLinear Network: input depth is ' +
+				inD + ' but should be ' + weights.dims[1]);
+		}
+		var h = img.dims[1];
+		var w = img.dims[2];
+		var outD = weights.dims[0];
+
+		var outImg = new Tensor([outD, h, w]);
+
+		for (var y = 0; y < h; y++) {
+			for (var x = 0; x < w; x++) {
+				for (var od = 0; od < outD; od++) {
+					var outIdx = x+w*(y+h*od);
+					outImg.data[outIdx] = biases.data[od];
+					for (var id = 0; id < inD; id++) {
+						var wIdx = id + od*inD;
+						var wt = weights.data[wIdx];
+						var inIdx = x+w*(y+h*id);
+						var imval = img.data[inIdx];
+						outImg.data[outIdx] += wt * imval;
+					}
+				}
+			}
+		}
+
+		return outImg;
+	},
+	backward: function(img, weights, biases) {
+		var imgp = ad.value(img);
+		var weightsp = ad.value(weights);
+		var biasesp = ad.value(biases);
+		var imgIs = img !== imgp;
+		var weightsIs = weights !== weightsp;
+		var biasesIs = biases !== biasesp;
+		var h = imgp.dims[1];
+		var w = imgp.dims[2];
+		var outD = weightsp.dims[0];
+		var inD = weightsp.dims[1];
+
+		for (var y = 0; y < h; y++) {
+			for (var x = 0; x < w; x++) {
+				for (var od = 0; od < outD; od++) {
+					var outIdx = x+w*(y+h*od);
+					var thisdx = this.dx.data[outIdx];
+					if (biasesIs) {
+						biases.dx.data[od] += thisdx;
+					}
+					for (var id = 0; id < inD; id++) {
+						var wIdx = id + od*inD;
+						var inIdx = x+w*(y+h*id);
+						if (weightsIs) {
+							var imval = imgp.data[inIdx];
+							weights.dx.data[wIdx] += imval * thisdx;
+						}
+						if (imgIs) {
+							var wt = weightsp.data[wIdx];
+							img.dx.data[inIdx] += wt * thisdx;
+						}
+					}
+				}
+			}
+		}
+	},
+	getParents: ad.naryGetParents
+});
+
+
+LayerwiseLinearNetwork.prototype.eval = function(img) {
+	var weights = this.isTraining ? this.weights : ad.value(this.weights);
+	var biases = this.isTraining ? this.biases : ad.value(this.biases);
+	return layerwiseMVMulAdd(img, weights, biases);
+};
+
+
+function layerwiseLinear(nIn, nOut, optname) {
+	return new LayerwiseLinearNetwork(nIn, nOut, optname);
+}
+
+
+// ----------------------------------------------------------------------------
+
+
+module.exports = {
+	linear: linear,
+	layerwiseLinear: layerwiseLinear
+};
+
+
 
