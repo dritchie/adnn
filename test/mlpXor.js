e78db5ee06e61fe8755c3f2229cf2174c8bcdec6
ccckmit <ccckmit@gmail.com>
2017-07-14 08:06:06 +0800

    add test/mlp.js mlpXor.js

---
+++ test/mlpXor.js
@@ -0,0 +1,38 @@
+var Tensor = require('../tensor');
+var ad = require('../ad');
+var nn = require('../nn');
+var opt = require('../opt');
+
+var nInputs = 2
+var nHidden = 5
+var nClasses = 2
+
+var net = nn.sequence([
+  nn.linear(nInputs, nHidden),
+  nn.tanh,
+  nn.linear(nHidden, nClasses),
+  nn.softmax
+])
+
+var data = [{ input: [0, 0], output: [0] },
+            { input: [0, 1], output: [1] },
+            { input: [1, 0], output: [1] },
+            { input: [1, 1], output: [0] }]
+
+var trainingData = nn.loadData(data)
+
+console.log('nnTrain')
+opt.nnTrain(net, trainingData, opt.classificationLoss, {
+  batchSize: 1,
+  iterations: 1000,
+  method: opt.sgd({ stepSize: 1, stepSizeDecay: 0.999 }),
+//  verbose: true
+})
+
+console.log('predict')
+// Predict class probabilities for new, unseen features
+for (let i = 0; i < trainingData.length; i++) {
+  var input = trainingData[i].input
+  var probs = net.eval(input)
+  console.log('input=%j\nprobs=%j', input, probs)
+}
